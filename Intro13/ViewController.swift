edfc43b3fc994929dfd94912825a7e33633b23d9
SwiftiCode <swifticode@203.203-211-151.static.qala.com.sg>
2016-08-01 18:22:45 +0800

    Initial Setup

--- Intro13/ViewController.swift
+++ Intro13/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var myRatingControl: RatingControl!
 
+    
+    // MARK: Default Template
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
