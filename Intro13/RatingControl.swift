f25731c9865d605c05f71fcf160997d39fdb3274
SwiftiCode <swifticode@203.203-211-151.static.qala.com.sg>
2016-08-01 18:18:57 +0800

    Initial

---
+++ Intro13/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  Intro13
+//
+//  Created by SwiftiCode on 1/8/16.
+//  Copyright © 2016 SwiftiCode. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
